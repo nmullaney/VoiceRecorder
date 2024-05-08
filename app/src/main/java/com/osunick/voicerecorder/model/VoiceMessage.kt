41d332d1b12d3217a5d271d8d62cc719d975a621
nmullaney <nora.mullaney@gmail.com>
2024-05-07 21:22:03 -0700

    Add the ability to delete a log (with a confirm dialog)

--- app/src/main/java/com/osunick/voicerecorder/model/VoiceMessage.kt
+++ app/src/main/java/com/osunick/voicerecorder/model/VoiceMessage.kt
@@ -2,4 +2,4 @@ package com.osunick.voicerecorder.model
 
 import java.time.LocalDateTime
 
-data class VoiceMessage(val text: String, val dateTime: LocalDateTime)
+data class VoiceMessage(val id: Int? = null, val text: String, val dateTime: LocalDateTime)
