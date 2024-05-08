e8d04da23f07b2a18a63ea6c4457831789c505cf
nmullaney <nora.mullaney@gmail.com>
2024-05-08 11:48:55 -0700

    Change state of Record widget button during record; add open button

--- app/src/main/java/com/osunick/voicerecorder/VoiceRecorderWidget.kt
+++ app/src/main/java/com/osunick/voicerecorder/VoiceRecorderWidget.kt
@@ -22,7 +22,7 @@ import javax.inject.Inject
  * Implementation of App Widget functionality.
  */
 @AndroidEntryPoint
-class VoiceRecorderWidget() : AppWidgetProvider() {
+class VoiceRecorderWidget : AppWidgetProvider() {
 
     @Inject
     lateinit var messageRepository: VoiceMessageRepository
@@ -43,8 +43,12 @@ class VoiceRecorderWidget() : AppWidgetProvider() {
 
         // Set the click listener for the button
         views.setOnClickPendingIntent(
-            R.id.appwidget_button,
+            R.id.record_button,
             getPendingSelfIntent(context, RECORD_ACTION)
+        )
+        views.setOnClickPendingIntent(
+            R.id.open_button,
+            openAppPendingIntent(context)
         )
         appWidgetManager.updateAppWidget(
             ComponentName(
@@ -68,11 +72,19 @@ class VoiceRecorderWidget() : AppWidgetProvider() {
         return PendingIntent.getBroadcast(context, 0, intent, PendingIntent.FLAG_IMMUTABLE)
     }
 
+    private fun openAppPendingIntent(context: Context): PendingIntent {
+        val intent = Intent(context, VoiceRecorderActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        }
+        return PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_IMMUTABLE)
+    }
+
     // Handle the button click action here
     override fun onReceive(context: Context, intent: Intent) {
         super.onReceive(context, intent)
         Log.d(TAG, "onReceive: ${intent.action}")
         if (RECORD_ACTION == intent.action) {
+            updateUIForRecordingStart(context)
             Log.d(TAG, "onReceive: handling ${intent.action}")
             speechRecognizer = VRSpeechRecognizer(context, object: OnSpeechEventListener {
                 override fun onRecognitionNotAvailable() {
@@ -81,6 +93,7 @@ class VoiceRecorderWidget() : AppWidgetProvider() {
                 }
 
                 override fun onSpeechRecognized(speech: String) {
+                    updateUIForRecordingStop(context)
                     Log.d(TAG, "Got speech: $speech")
                     // it would be nicer to do this in a WorkManager, but this will work
                     runBlocking {
@@ -101,6 +114,39 @@ class VoiceRecorderWidget() : AppWidgetProvider() {
         }
     }
 
+    private fun updateUIForRecordingStop(context: Context) {
+        val views = RemoteViews(context.packageName, R.layout.voice_recorder_widget)
+        views.setTextViewText(R.id.record_button, context.getString(R.string.record))
+        views.setInt(
+            R.id.record_button,
+            "setBackgroundColor",
+            context.getColor(R.color.light_blue_600))
+        val appWidgetManager = AppWidgetManager.getInstance(context)
+        appWidgetManager.updateAppWidget(
+            ComponentName(
+                context,
+                VoiceRecorderWidget::class.java
+            ), views
+        )
+    }
+
+    private fun updateUIForRecordingStart(context: Context) {
+        val views = RemoteViews(context.packageName, R.layout.voice_recorder_widget)
+        views.setTextViewText(R.id.record_button, context.getString(R.string.recording))
+        views.setInt(
+            R.id.record_button,
+            "setBackgroundColor",
+            context.getColor(R.color.teal_200))
+        val appWidgetManager = AppWidgetManager.getInstance(context)
+        appWidgetManager.updateAppWidget(
+            ComponentName(
+                context,
+                VoiceRecorderWidget::class.java
+            ), views
+        )
+    }
+
+
     override fun onDeleted(context: Context?, appWidgetIds: IntArray?) {
         super.onDeleted(context, appWidgetIds)
         if (this::speechRecognizer.isInitialized) {
@@ -119,10 +165,10 @@ internal fun updateAppWidget(
     appWidgetManager: AppWidgetManager,
     appWidgetId: Int
 ) {
-    val widgetText = context.getString(R.string.appwidget_text)
+    val widgetText = context.getString(R.string.record)
     // Construct the RemoteViews object
     val views = RemoteViews(context.packageName, R.layout.voice_recorder_widget)
-    views.setTextViewText(R.id.appwidget_button, widgetText)
+    views.setTextViewText(R.id.record_button, widgetText)
 
     // Instruct the widget manager to update the widget
     appWidgetManager.updateAppWidget(appWidgetId, views)
